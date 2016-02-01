a2bbe8707abbc9939d54a2d2e9c2ca03ff6d33b0
Edvin Salo <CodeGeek@Edvins-Mac.local>
2016-02-01 22:56:07 +0100

    Angular used for video gettin on home page

--- public/javascripts/coa.js
+++ public/javascripts/coa.js
@@ -1,11 +1,21 @@
-var app = angular.module('Coa', ['ngRoute']);
+var app = angular.module('Coa', ['ngResource', 'ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 		.when('/', {
-			templateUrl: 'partials/home.html'
+			templateUrl: 'partials/home.html',
+			controller: 'HomeCtrl'
 		})
 		.otherwise({
 			redirectTo: '/'
 		});
 }]);
+
+app.controller('HomeCtrl', ['$scope', '$resource', 
+	function($scope, $resource) {
+		var Videos = $resource('/videos');
+		Videos.query(function(videos) {
+			$scope.videos = videos;
+		});
+	}
+]);
